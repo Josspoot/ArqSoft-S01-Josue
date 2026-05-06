742237da61350b174b192bbd377ea58c688c50ff
Josue Enmanuel Poot Mateo <josue.poot@tecdesoftware.edu.mx>
2026-05-06 10:43:47 -0600

    ?

---
+++ WebApplication2/Models/Item.cs
@@ -0,0 +1,11 @@
+namespace WebApplication2.Models;
+
+public class Item
+{
+    public int Id {get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public int Ano { get; set; }
+    public string Consola { get; set; }
+    public string Descripcion{ get; set; }
+}
